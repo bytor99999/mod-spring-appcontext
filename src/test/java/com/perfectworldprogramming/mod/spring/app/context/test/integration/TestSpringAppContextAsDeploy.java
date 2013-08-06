f08732d2f861519339bf46b91ca2a865896a5cc4
Mark Spritzler <mark@perfectworldprogramming.com>
2013-08-06 15:45:50 -0700

    Interestingly, it worked deployed as a Verticle, but not as a Module.

--- src/test/java/com/perfectworldprogramming/mod/spring/app/context/test/integration/TestSpringAppContextAsDeploy.java
+++ src/test/java/com/perfectworldprogramming/mod/spring/app/context/test/integration/TestSpringAppContextAsDeploy.java
@@ -19,38 +19,69 @@ import org.vertx.testtools.TestVerticleInfo;
 @TestVerticleInfo(includes = "bytor99999~mod-spring-appcontext~1.0.0-RC2")
 public class TestSpringAppContextAsDeploy extends TestVerticle{
 
-  ApplicationContext context;
+    ApplicationContext context;
 
-  @Test
-  public void testStaticAppContext() {
-    initialize();
+    @Test
+    public void testStaticAppContext() {
+        initialize();
 
-    JsonObject configFiles = new JsonObject();
-    JsonArray xmlFilesArray = new JsonArray();
-    xmlFilesArray.add("spring/testing-separate-module-config.xml");
+        JsonObject configFiles = new JsonObject();
+        JsonArray xmlFilesArray = new JsonArray();
+        xmlFilesArray.add("spring/testing-separate-module-config.xml");
 
-    configFiles.putArray("configFiles", xmlFilesArray);
-    configFiles.putString("configType", ConfigType.XML.getValue());
+        configFiles.putArray("configFiles", xmlFilesArray);
+        configFiles.putString("configType", ConfigType.XML.getValue());
 
-    container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.SpringAppContextVerticle"      ,//"bytor99999~mod-spring-appcontext~1.0.0-RC2",
-      configFiles,
-      new Handler<AsyncResult<String>>() {
-        @Override
-        public void handle(AsyncResult<String> event) {
-          context = SpringAppContextVerticle.getApplicationContext();
-          String[] beanNames = context.getBeanDefinitionNames();
-          for (String name: beanNames) {
-            System.out.println(name);
-          }
-          container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.test.integration",
-              new Handler<AsyncResult<String>>() {
+        container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.SpringAppContextVerticle",
+            configFiles,
+            new Handler<AsyncResult<String>>() {
                 @Override
                 public void handle(AsyncResult<String> event) {
-                  vertx.eventBus().send("test", "Hello World");
+                    context = SpringAppContextVerticle.getApplicationContext();
+                    if (context == null) {
+                        System.out.println("Doesn't work deploying as just a Verticle. context is null");
+                    } else {
+                        String[] beanNames = context.getBeanDefinitionNames();
+                        for (String name: beanNames) {
+                            System.out.println(name);
+                        }
+                        container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.test.integration.StaticMemberVerticle",
+                            new Handler<AsyncResult<String>>() {
+                                @Override
+                                public void handle(AsyncResult<String> event) {
+                                    vertx.eventBus().send("test", "Hello World");
+                                }
+                            }
+                        );
+                    }
                 }
-              });
-        }
-      }
-    );
-  }
+            }
+        );
+
+        container.deployModule("bytor99999~mod-spring-appcontext~1.0.0-RC2",
+            configFiles,
+            new Handler<AsyncResult<String>>() {
+                @Override
+                public void handle(AsyncResult<String> event) {
+                    context = SpringAppContextVerticle.getApplicationContext();
+                    if (context == null) {
+                        System.out.println("Doesn't work deploying as Module. context is null");
+                    } else {
+                        String[] beanNames = context.getBeanDefinitionNames();
+                        for (String name : beanNames) {
+                            System.out.println(name);
+                        }
+                        container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.test.integration.StaticMemberVerticle",
+                            new Handler<AsyncResult<String>>() {
+                                @Override
+                                public void handle(AsyncResult<String> event) {
+                                  vertx.eventBus().send("test", "Hello World");
+                                }
+                            }
+                        );
+                    }
+                }
+            }
+        );
+    }
 }
