418e49e2e887ba6e86d758ece8a14bad8dc75631
Mark Spritzler <mark@perfectworldprogramming.com>
2013-08-06 16:10:37 -0700

    More testing where I can access the ApplicationContext. And what is really interesting is that you have to deployModule and deployVerticle to get it to work.

--- src/test/java/com/perfectworldprogramming/mod/spring/app/context/test/integration/StaticMemberVerticle.java
+++ src/test/java/com/perfectworldprogramming/mod/spring/app/context/test/integration/StaticMemberVerticle.java
@@ -1,5 +1,7 @@
 package com.perfectworldprogramming.mod.spring.app.context.test.integration;
 
+import com.perfectworldprogramming.mod.spring.app.context.SpringAppContextVerticle;
+import org.springframework.context.ApplicationContext;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.platform.Verticle;
@@ -13,11 +15,23 @@ public class StaticMemberVerticle extends Verticle {
 
   @Override
   public void start() {
-    vertx.eventBus().registerHandler("test", new Handler<Message>() {
-      @Override
-      public void handle(Message event) {
-        System.out.println("I received an event: " + event.body().toString());
-      }
-    });
+      vertx.eventBus().registerHandler("test", new Handler<Message>() {
+          @Override
+          public void handle(Message event) {
+              System.out.println("I received an event: " + event.body().toString());
+              ApplicationContext context = SpringAppContextVerticle.getApplicationContext();
+              if (context == null) {
+                  System.out.println("Doesn't work inside another project's Verticle. context is null");
+
+              } else {
+                  System.out.println("****************************************************");
+                  System.out.println("Accessing the ApplicationContext in another Verticle/Module");
+                  String[] beanNames = context.getBeanDefinitionNames();
+                  for (String name: beanNames) {
+                      System.out.println(name);
+                  }
+              }
+           }
+      });
   }
 }
--- src/test/java/com/perfectworldprogramming/mod/spring/app/context/test/integration/TestSpringAppContextAsDeploy.java
+++ src/test/java/com/perfectworldprogramming/mod/spring/app/context/test/integration/TestSpringAppContextAsDeploy.java
@@ -10,6 +10,7 @@ import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.testtools.TestVerticle;
 import org.vertx.testtools.TestVerticleInfo;
+import org.vertx.testtools.VertxAssert;
 
 /**
  * User: Mark Spritzler
@@ -20,6 +21,8 @@ import org.vertx.testtools.TestVerticleInfo;
 public class TestSpringAppContextAsDeploy extends TestVerticle{
 
     ApplicationContext context;
+
+    int count = 0;
 
     @Test
     public void testStaticAppContext() {
@@ -32,6 +35,41 @@ public class TestSpringAppContextAsDeploy extends TestVerticle{
         configFiles.putArray("configFiles", xmlFilesArray);
         configFiles.putString("configType", ConfigType.XML.getValue());
 
+
+        container.deployModule("bytor99999~mod-spring-appcontext~1.0.0-RC2",
+            configFiles,
+            new Handler<AsyncResult<String>>() {
+                @Override
+                public void handle(AsyncResult<String> event) {
+                    context = SpringAppContextVerticle.getApplicationContext();
+                    if (context == null) {
+                        System.out.println("Doesn't work deploying as Module. context is null");
+                        count++;
+                        if (count == 2) {
+                            VertxAssert.testComplete();
+                        }
+                    } else {
+                        String[] beanNames = context.getBeanDefinitionNames();
+                        for (String name : beanNames) {
+                            System.out.println(name);
+                        }
+                        container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.test.integration.StaticMemberVerticle",
+                            new Handler<AsyncResult<String>>() {
+                                @Override
+                                public void handle(AsyncResult<String> event) {
+                                    vertx.eventBus().send("test", "Hello World");
+                                    count++;
+                                    if (count == 2) {
+                                      VertxAssert.testComplete();
+                                    }
+                                }
+                            }
+                        );
+                    }
+                }
+            }
+        );
+
         container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.SpringAppContextVerticle",
             configFiles,
             new Handler<AsyncResult<String>>() {
@@ -40,6 +78,10 @@ public class TestSpringAppContextAsDeploy extends TestVerticle{
                     context = SpringAppContextVerticle.getApplicationContext();
                     if (context == null) {
                         System.out.println("Doesn't work deploying as just a Verticle. context is null");
+                        count++;
+                        if (count == 2) {
+                            VertxAssert.testComplete();
+                        }
                     } else {
                         String[] beanNames = context.getBeanDefinitionNames();
                         for (String name: beanNames) {
@@ -50,32 +92,10 @@ public class TestSpringAppContextAsDeploy extends TestVerticle{
                                 @Override
                                 public void handle(AsyncResult<String> event) {
                                     vertx.eventBus().send("test", "Hello World");
-                                }
-                            }
-                        );
-                    }
-                }
-            }
-        );
-
-        container.deployModule("bytor99999~mod-spring-appcontext~1.0.0-RC2",
-            configFiles,
-            new Handler<AsyncResult<String>>() {
-                @Override
-                public void handle(AsyncResult<String> event) {
-                    context = SpringAppContextVerticle.getApplicationContext();
-                    if (context == null) {
-                        System.out.println("Doesn't work deploying as Module. context is null");
-                    } else {
-                        String[] beanNames = context.getBeanDefinitionNames();
-                        for (String name : beanNames) {
-                            System.out.println(name);
-                        }
-                        container.deployVerticle("com.perfectworldprogramming.mod.spring.app.context.test.integration.StaticMemberVerticle",
-                            new Handler<AsyncResult<String>>() {
-                                @Override
-                                public void handle(AsyncResult<String> event) {
-                                  vertx.eventBus().send("test", "Hello World");
+                                    count++;
+                                    if (count == 2) {
+                                      VertxAssert.testComplete();
+                                    }
                                 }
                             }
                         );
